3fd2611dbd3186f2ab7e4f8c2ee7250d182695d2
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-08-27 11:38:55 -0300

    Adicionado descriçoes sobre imagens, espaçamento e bordas

--- app04/lib/widget_carro.dart
+++ app04/lib/widget_carro.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
 class WidgetCarro extends StatelessWidget {
-  const WidgetCarro({Key? key}) : super(key: key);
+  // Atributos que serão utilizados para receber
+  // informações
+  final String marca;
+  final String modelo;
+  final String foto;
+
+  // Construtor
+  // o primeiro método que é executado quando o objeto
+  // da classe é instanciado
+  const WidgetCarro(this.marca, this.modelo, this.foto, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      height: 300,
-      color: Colors.blue[100],
+      // Espaçamento (interno)
+      padding: EdgeInsets.all(30),
+
+      // Margens (externo)
       margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+
+      // Decoração
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.blue.shade900,
+          width: 1,
+        ),
+        color: Colors.blue[100],
+      ),
+
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 300,
+
+      child: Column(
+        children: [
+          Text(
+            this.marca,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            this.modelo,
+            style: TextStyle(
+              fontSize: 20,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
