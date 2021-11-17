8d0d3a3252145befd794393a1b0f72561cbbbfde
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-11-17 12:21:15 -0300

    Atualização de cadastro de cafes (CRUD)

--- app09_loja_cafe/lib/main.dart
+++ app09_loja_cafe/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -33,6 +34,34 @@ class PrincipalPage extends StatefulWidget {
 }
 
 class _PrincipalPageState extends State<PrincipalPage> {
+  // Referenciar a coleção do Firestore
+  late CollectionReference cafes;
+  @override
+  void initState() {
+    super.initState();
+    cafes = FirebaseFirestore.instance.collection('cafes');
+  }
+
+  // Especificar a aparencia de cada elemento da lista
+  exibirItemColecao(item) {
+    String nome = item.data()['nome'];
+    String preco = item.data()['preco'];
+    return ListTile(
+      title: Text(nome, style: const TextStyle(fontSize: 30)),
+      subtitle: Text('R\$ $preco', style: const TextStyle(fontSize: 25)),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () {
+          // Apagar um documento
+          cafes.doc(item.id).delete();
+        },
+      ),
+      onTap: () {
+        Navigator.pushNamed(context, '/cadastro', arguments: item.id);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +80,35 @@ class _PrincipalPageState extends State<PrincipalPage> {
         ],
       ),
       backgroundColor: Colors.brown.shade100,
-      body: Container(),
+
+      // Listar os documentos da coleção
+
+      body: StreamBuilder<QuerySnapshot>(
+          // Fonte de dados
+          stream: cafes.snapshots(),
+          // Exibir os dados retornados
+          builder: (context, snapshot) {
+            // Verificar o estado da conexão
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Center(
+                  child: Text('Não foi possivel conectar ao Firestore'),
+                );
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              // Se os dados forem recebidos com sucesso
+              default:
+                final dados = snapshot.requireData;
+                return ListView.builder(
+                  itemCount: dados.size,
+                  itemBuilder: (context, index) {
+                    return exibirItemColecao(dados.docs[index]);
+                  },
+                );
+            }
+          }),
     );
   }
 }
